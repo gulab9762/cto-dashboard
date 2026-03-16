9bb0fa74900584670ffe3d8fecf1751f2fd366c0
Gulab <gulabdhapa2001@gmail.com>
2026-03-16 20:28:49 +0530

    chore: delete an unspecified file.

--- integration-service/src/modules/jira.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { JiraController } from "../controllers/jira.controller";
-import { KafkaModule } from "./kafka.module";
-
-@Module({
-  imports: [KafkaModule],
-  controllers: [JiraController],
-})
-export class JiraModule {}
